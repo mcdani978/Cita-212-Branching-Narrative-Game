585f2dfd98764cdbbc3135971caa59dd4d22425b
mcdani978 <mcdani978@morrisville.edu>
2023-11-20 17:40:13 -0500

    First and only Commit

---
+++ Text101-master/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text txtComponent;
+    [SerializeField] State startingState;
+
+    // our current state
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        txtComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextState = state.GetNextState();
+        for (int i = 0; i < nextState.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextState[i];
+            }
+        }
+
+        txtComponent.text = state.GetStateStory();
+    } // ManageState
+}  // AdventureGame
---
+++ Text101-master/Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14,10)]  [SerializeField] string strStoryText;
+    [SerializeField] State[] nextState;
+
+    public string GetStateStory()
+    {
+        return strStoryText;
+    }
+
+    public State[] GetNextState()
+    {
+        return nextState;
+    }
+}
